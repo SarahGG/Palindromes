edd58b0bd2e68aee4af0716ac70b132e29895c50
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-06 22:16:49 -0400

    Added startIndex variable to PalindromeLens, plus associated getters, setters, and test

--- src/com/company/PalindromeLens.java
+++ src/com/company/PalindromeLens.java
@@ -2,8 +2,17 @@ package com.company;
 
 public class PalindromeLens {
 
+    private int startIndex;
+
     public PalindromeLens() {
+        startIndex = 0;
     }
 
+    public int getStartIndex() {
+        return startIndex;
+    }
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
 
 }
--- test/PalindromeLensTest.java
+++ test/PalindromeLensTest.java
@@ -1,4 +1,18 @@
+import com.company.PalindromeLens;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class PalindromeLensTest {
+    private PalindromeLens blankPalindromeLens = new PalindromeLens();
 
+    @Test
+    public void settingStartIndexTo2ReturnsStartIndexOf2() {
+        blankPalindromeLens.setStartIndex(2);
+        Assert.assertEquals(blankPalindromeLens.getStartIndex(), 2);
+    }
+
+    @Test
+    public void initializingBlankLensReturnsStartIndexOf0() {
+        Assert.assertEquals(blankPalindromeLens.getStartIndex(), 0);
+    }
 }
