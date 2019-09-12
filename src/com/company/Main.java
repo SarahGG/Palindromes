ed18f33450e9768c1c536f3d761f76545129ae90
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-11 23:41:39 -0400

    Added main method interaction so user can use the program.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String motherString;
+
+        System.out.print("Enter a string to be analyzed: ");
+        motherString = scanner.nextLine();
+        System.out.println();
+
+        int palindromes = PalindromeQA.calculatePalindromes(motherString); // 11 substring palindromes, 13 potential palindromes, 21 substrings
+
+        System.out.printf("String %s has %d palindromes%n", motherString, palindromes);
     }
 }
