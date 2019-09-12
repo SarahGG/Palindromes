3160dff01e7585da22525503c451b084650eabc6
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-11 23:39:02 -0400

    Added calculatePalindromes method to PalindromeQA class, with associated tests

--- src/com/company/PalindromeQA.java
+++ src/com/company/PalindromeQA.java
@@ -1,6 +1,23 @@
 package com.company;
 
 public class PalindromeQA {
+
+    public static int calculatePalindromes(String motherString) {
+        int palindromes = 0;
+
+        for (int i = 0; i < motherString.length(); i++) { //
+            for(int j = i + 1; j <= motherString.length(); j++) {
+
+                    if(PalindromeQA.isPalindrome(motherString.substring(i, j))) {
+                        palindromes++;
+                    }
+            }
+
+        }
+
+        return palindromes;
+    }
+
     public static boolean isPalindrome(String string) {
 
         for(int i = 0; i < (string.length() / 2); i++) {   // iterates on the string, looking at a the first and last letter in a progressively shrinking window.
--- test/PalindromeQATest.java
+++ test/PalindromeQATest.java
@@ -5,6 +5,17 @@ import org.junit.Test;
 public class PalindromeQATest {
 
     @Test
+    public void ABBAReturns6() {
+        int palindromes = PalindromeQA.calculatePalindromes("ABBA");
+        Assert.assertEquals(palindromes, 6);
+    }
+
+    @Test
+    public void ABABAReturns9() {
+        int palindromes = PalindromeQA.calculatePalindromes("ABABA");
+    }
+
+    @Test
     public void TACOCATIsPalindromeReturnsTrue() {
         boolean isPalindrome = PalindromeQA.isPalindrome("TACOCAT");
         Assert.assertTrue(isPalindrome);
@@ -15,5 +26,4 @@ public class PalindromeQATest {
         boolean isPalindrome = PalindromeQA.isPalindrome("TACODOG");
         Assert.assertFalse(isPalindrome);
     }
-
 }
