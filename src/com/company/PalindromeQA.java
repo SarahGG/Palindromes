38a71c15400ac09196b80549d42a3b2f3e150a7f
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-11 23:38:08 -0400

    Added isPalindrome method to PalindromeQA class, with associated tests

--- src/com/company/PalindromeQA.java
+++ src/com/company/PalindromeQA.java
@@ -1,5 +1,18 @@
 package com.company;
 
 public class PalindromeQA {
-    
+    public static boolean isPalindrome(String string) {
+
+        for(int i = 0; i < (string.length() / 2); i++) {   // iterates on the string, looking at a the first and last letter in a progressively shrinking window.
+
+            char a = string.charAt(i); // first char in the window
+            char b = string.charAt(string.length() - (1 + i)); // last char in the window
+
+            if (!(a == b)) {   // if the two compared characters don't match, this isn't a palindrome
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
--- test/PalindromeQATest.java
+++ test/PalindromeQATest.java
@@ -4,5 +4,16 @@ import org.junit.Test;
 
 public class PalindromeQATest {
 
+    @Test
+    public void TACOCATIsPalindromeReturnsTrue() {
+        boolean isPalindrome = PalindromeQA.isPalindrome("TACOCAT");
+        Assert.assertTrue(isPalindrome);
     }
+
+    @Test
+    public void TACODOGIsPalindromeReturnsFalse() {
+        boolean isPalindrome = PalindromeQA.isPalindrome("TACODOG");
+        Assert.assertFalse(isPalindrome);
+    }
+
 }
